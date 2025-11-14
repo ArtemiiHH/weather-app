a3783b2527ee43d4e60145a95cba8b4588d697ac
ArtemiiHH <artemiibarashkov2003@gmail.com>
2025-11-14 15:52:52 +0200

    Fix api module, keep basic for now

--- src/modules/api.js
+++ src/modules/api.js
@@ -1,13 +1,15 @@
 export async function getWeather(city) {
   try {
     const response = await fetch(
-      "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Moscow?unitGroup=metric&key=FWRFKQ837QJ9VNNBZK4DKNPY6&contentType=json"
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=FWRFKQ837QJ9VNNBZK4DKNPY6`
     );
-    const data = await response.json();
 
+    // Error handling
     if (!response.ok) {
       throw new Error("City or location was not found.");
     }
+
+    const data = await response.json();
 
     return data;
   } catch (error) {
