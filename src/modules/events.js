e3d36c0a4457e9ea926468e0d6b07143f55c0a90
ArtemiiHH <artemiibarashkov2003@gmail.com>
2025-11-14 16:31:13 +0200

    Put everything inside an events func

--- src/modules/events.js
+++ src/modules/events.js
@@ -1,2 +1,6 @@
-import { input } from "./dom";
+import { input, input } from "./dom";
 import { getWeather } from "./api";
+
+export async function events() {
+  document.querySelector(".search-btn").addEventListener("click", () => {});
+}
