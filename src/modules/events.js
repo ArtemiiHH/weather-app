179866afb6bc37a419b7534f140ce80b71699d46
ArtemiiHH <artemiibarashkov2003@gmail.com>
2025-11-14 19:13:21 +0200

    Add async func to event listener + fill logic inside

--- src/modules/events.js
+++ src/modules/events.js
@@ -1,6 +1,22 @@
-import { input } from "./dom";
+import { input, showError, updateWeather } from "./dom";
 import { getWeather } from "./api";
 
-export async function events() {
-  document.querySelector(".search-btn").addEventListener("click", () => {});
+// Validate inputs here, later
+
+// Events function
+export function events() {
+  document.querySelector(".search-btn").addEventListener("click", async () => {
+    // Input value
+    const city = input.value;
+    // Wait for data
+    const data = await getWeather(city);
+
+    // If API doesnt return valid data:
+    if (!data || data === null) {
+      showError();
+    } else {
+      // If it does, update DOM module with it
+      updateWeather(data);
+    }
+  });
 }
